32d43ebdde235e7f9cbb39e5acd24eccb1601d9b
MORI Shingo <cos@kmc.gr.jp>
2012-02-11 14:52:49 +0900

    add 3/1.cpp

---
+++ 3/1.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void Usage(int argc, char *argv[]) {
+  printf("Usage: %s Keyword\n", argv[0]);
+  exit(1);
+}
+
+const int N = 300000;
+string randStr;
+void MakeRandStr(unsigned int r) {
+  randStr.resize(N);
+  for (int i = 0; i < N; i++) {
+    r =  (r * 1103515245 + 12345) & 0xFFFFFFFF;
+    randStr[i] =  0x61 + (26 * (r / 0x10000)) / 0x10000;
+  }
+}
+
+bool OK(int skip, int start, const string &keyword, const string &str) {
+  for (int i = 0; i < (int)keyword.size(); i++) {
+    int index = start + i * skip;
+    if (keyword[i] != randStr[index]) { return false; }
+  }
+  return true;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) { Usage(argc, argv); }
+  MakeRandStr(16);
+  string keyword = argv[1];
+  string revKeyword = keyword;
+  reverse(revKeyword.begin(), revKeyword.end());
+  int n = randStr.size();
+  int len = keyword.size();
+  for (int skip = 1; skip <= (n + len - 1) / len; skip++) {
+    int use = 1 + (len - 1) * skip;
+    for (int start = 0; start <= n - use; start++) {
+      if (OK(skip, start, keyword, randStr) ||
+          OK(skip, start, revKeyword, randStr)) {
+        printf("%d,%d\n", skip, start);
+      }
+    }
+  }
+}
